a3630d9cf320e5a3f18d0ca7e40e1ea14e86be81
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-18 18:14:05 +0700

    navbar sementara

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,64 +1,91 @@
 "use client"
-import { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
+
+interface FormDataState {
+  title: string;
+  link: string;
+  pembuat: string;
+  image: File | null;
+}
 
 const NewArticle = () => {
-  const [preview, setPreview] = useState(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const [formData, setFormData] = useState<FormDataState>({
+    title: "",
+    link: "",
+    pembuat: "",
+    image: null,
+  });
 
-  const previewPhoto = (event : any) => {
-    const file = event.target.files[0];
+  const previewPhoto = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
     if (file) {
+      setFormData((prev) => ({ ...prev, image: file }));
       const fileReader = new FileReader();
-      fileReader.onload = (e: any) => {
-        setPreview(e.target.result);
+      fileReader.onload = (e) => {
+        setPreview(e.target?.result as string);
       };
       fileReader.readAsDataURL(file);
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append("title", formData.title);
+    data.append("link", formData.link);
+    data.append("pembuat", formData.pembuat);
+    if (formData.image) {
+      data.append("image", formData.image);
+    }
+
+    try {
+      const response = await fetch("/api/post", {
+        method: "POST",
+        body: data,
+      });
+      
+      if (response.ok) {
+        alert("Artikel berhasil dikirim!");
+      } else {
+        alert("Gagal mengirim artikel.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Terjadi kesalahan.");
     }
   };
 
   return (
     <div className="container mt-5">
       <h1 className="display-4">Artikel Baru</h1>
-      <form id="babForm" action="/new" method="post" encType="multipart/form-data">
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="form-group">
           <label htmlFor="title">Title:</label>
-          <input type="text" autoComplete="false" id="title" name="title" className="form-control" />
+          <input type="text" id="title" name="title" className="form-control" value={formData.title} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label htmlFor="link">Link Video (Optional):</label>
-          <input type="text" autoComplete="false" id="link" name="link" className="form-control" />
+          <input type="text" id="link" name="link" className="form-control" value={formData.link} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label htmlFor="pembuat">Pembuat (Optional):</label>
-          <input type="text" autoComplete="false" id="pembuat" name="pembuat" className="form-control" />
+          <input type="text" id="pembuat" name="pembuat" className="form-control" value={formData.pembuat} onChange={handleChange} />
         </div>
-        <input type="hidden" id="content" name="content" />
         <div className="form-group">
           <label htmlFor="image">Cover Image:</label>
           <input type="file" name="image" id="image" className="form-control-file" required onChange={previewPhoto} />
         </div>
         <div className="form-group">
           {preview && (
-            <img
-              src={preview}
-              style={{ width: "460px", maxWidth: "100%", border: "2px solid #ccc", objectFit: "cover", borderRadius: "24px" }}
-              className="img-fluid"
-              alt="Preview"
-            />
+            <img src={preview} style={{ width: "460px", maxWidth: "100%", border: "2px solid #ccc", objectFit: "cover", borderRadius: "24px" }} className="img-fluid" alt="Preview" />
           )}
         </div>
-        <button type="button" id="addBab" className="btn btn-primary">
-          Tambah Bab
-        </button>
-        <div id="babSections" className="mt-3"></div>
-        <button
-          data-sitekey="6LfslRQoAAAAAHVBGwEVitjEQSjCD6F8unKDUdct"
-          data-callback="onSubmit"
-          name="g-recaptcha-response"
-          id="postButton"
-          data-action="submit"
-          type="submit"
-          className="btn-lg g-recaptcha btn btn-info mt-3 rounded-lg"
-        >
+        <button type="submit" className="btn btn-info mt-3 rounded-lg">
           <i className="fa fa-paper-plane" aria-hidden="true"></i> Kirim
         </button>
       </form>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setIsVisible(prevScrollPos > currentScrollPos);
+      setPrevScrollPos(currentScrollPos);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  const openApp = () => {
+    const appUrl = `rejang://pedia${window.location.href.replace("https://rejangpedia.glitch.me", "")}`;
+    window.location.href = appUrl;
+  };
+
+  return (
+    <>
+      {/* Navbar Dekstop */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-light sticky-top" id="khususDekstop">
+        <Link className="navbar-brand" href="/">
+          rejangpedia
+        </Link>
+        <button className="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="fa fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav justify-content-center">
+            <li className="nav-item">
+              <Link className="nav-link" href="https://kamusrejang.glitch.me">
+                <i className="fa fa-book"></i> Kamus Rejang
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/new">
+                <i className="fa fa-plus"></i> Tambah Artikel
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className="fa fa-bars"></i> Daftar
+              </a>
+              <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                <Link className="dropdown-item" href="/tentang">
+                  <i className="fa fa-question"></i> Tentang
+                </Link>
+                <div className="dropdown-divider"></div>
+                <Link className="dropdown-item" href="https://mfathinhalim.github.io">
+                  <i className="fa fa-question-circle"></i> Tentang Fathin
+                </Link>
+                <button className="dropdown-item" onClick={openApp}>Buka di Aplikasi</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Navbar HP Atas */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-light sticky-top d-lg-none" id="khususHp">
+        <div className="container">
+          <div className="row w-100">
+            <div className="col-9 text-center">
+              <Link className="navbar-brand" href="/">
+                rejangpedia
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Navbar HP Bawah */}
+      <nav className={`navbar fixed-bottom navbar-expand navbar-light bg-light ${isVisible ? "" : "d-none"}`} id="khususHpBawah">
+        <ul className="navbar-nav nav-justified w-100">
+          <li className="nav-item">
+            <Link className="nav-link" href="/">
+              <i className="fa fa-home"></i>
+              <span className="d-block">Home</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/new">
+              <i className="fa fa-plus"></i>
+              <span className="d-block">Tambah</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="https://kamusrejang.glitch.me">
+              <i className="fa fa-book"></i>
+              <span className="d-block">Kamus</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/dropdown">
+              <i className="fa fa-bars"></i>
+              <span className="d-block">Menu</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
