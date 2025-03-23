66b3bc98942ebebdbc6f83231217b2b770741f25
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-23 17:30:28 +0700

    HIJAU

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const LoadingSpinner = () => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="spinner-border text-primary" role="status" style={{ width: "3rem", height: "3rem" }}>
+      <div className="spinner-border text-primar" role="status" style={{ width: "3rem", height: "3rem" }}>
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
