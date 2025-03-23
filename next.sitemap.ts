02095551f1d78f1ea41cf240c6b9148009aefc5b
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-23 20:15:51 +0700

    sitemap

---
+++ next.sitemap.ts
@@ -0,0 +1,19 @@
+import { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: process.env.SITE_URL || 'https://rejangpedia.vercel.app/',
+  generateRobotsTxt: true, // Membuat robots.txt secara otomatis
+  changefreq: 'daily', // Seberapa sering konten berubah
+  priority: 0.7, // Prioritas URL (skala 0-1)
+  sitemapSize: 5000, // Jumlah maksimum URL per file sitemap
+  generateIndexSitemap: true,
+  exclude: ['/admin', '/ongoing/*'], // Halaman yang tidak ingin disertakan
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: '*', disallow: ['/admin', '/ongoing'] },
+    ],
+  },
+};
+
+export default config;
