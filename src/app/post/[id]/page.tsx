54ce301c9f9f7a1e4c3fa04363a7f96ae41c49bb
M.Fathin Halim <halimfathin7@gmail.com>
2025-03-23 19:24:19 +0700

    meta

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import Head from "next/head";  // Import untuk meta tag
 import styles from './page.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faShare } from "@fortawesome/free-solid-svg-icons";
@@ -28,57 +29,115 @@ const ArticlePage = () => {
     return <LoadingSpinner />;
   }
 
+  const pageTitle = data.Title || "Artikel Menarik"; // Default title
+  const pageDescription = data.Content?.[0]?.babContent
+    ? data.Content[0].babContent.substring(0, 150) + "..."
+    : "Baca artikel lengkap di sini.";
+  const pageImage = data.Image || "/default-image.jpg"; // Default image jika tidak ada
+  const pageUrl = `https://www.yourwebsite.com/article/${id}`; // Ganti sesuai dengan domain Anda
+
   return (
-    <div className={`container d-flex justify-content-center`}>
-      <div className={styles.container}>
-        <div className="w-100 d-flex flex-column">
-          <h3 className="my-0" style={{ color: "var(--primary)", fontWeight: "bolder" }}>
-            {data.Edit && data.Edit !== "tidak ada waktu" ? `${data.Edit}` : "07 Maret 2023"}
-          </h3>
-          <h1 id="title">{data.Title}</h1>
+    <>
+      {/* Meta Tags */}
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="keywords" content="Rejangpedia, Artikel, Budaya, Bahasa Rejang" />
+        <meta name="author" content={data.Pembuat || "Admin"} />
 
-          <p className="dibuat mr-auto" style={{ fontSize: "15px", maxWidth: "100%", paddingLeft: "10px", borderLeft: "3px solid #424347" }}>
-            <span style={{ color: "var(--primary)" }}>Ditulis oleh</span> {data.Pembuat} {data.Waktu ? ` pada ${data.Waktu}` : " pada 07-Maret-2023"}
-            {data.Diedit && (
-              <>
-                <br />
-                <span style={{ color: "var(--secondary)" }}>Diedit oleh</span> {data.Diedit}
-              </>
-            )}
-          </p>
+        {/* Open Graph Meta Tags (untuk Facebook, Twitter, dll.) */}
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:image" content={pageImage} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:type" content="article" />
 
-          <div className="d-flex flex-column flex-md-row gap-3">
-            {data.Image && (
-              <img className="mr-2 cover" style={{ height: "250px", objectFit: "contain", background: "rgba(0, 0, 0, 0)", borderRadius: "12px" }} src={data.Image} alt={data.Title} />
-            )}
+        {/* Twitter Card Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageDescription} />
+        <meta name="twitter:image" content={pageImage} />
+      </Head>
 
-            {data.Link && (
-              <article>
-                <iframe style={{ width: "460px", maxWidth: "100%", height: "250px", objectFit: "cover", borderRadius: "12px" }} className="img-fluid" src={data.Link} title={data.Title}></iframe>
-              </article>
-            )}
-          </div>
+      <div className={`container d-flex justify-content-center`}>
+        <div className={styles.container}>
+          <div className="w-100 d-flex flex-column">
+            <h3 className="my-0" style={{ color: "var(--primary)", fontWeight: "bolder" }}>
+              {data.Edit && data.Edit !== "tidak ada waktu" ? `${data.Edit}` : "07 Maret 2023"}
+            </h3>
+            <h1 id="title">{data.Title}</h1>
 
-          <div className="d-flex mt-3 gap-2">
-            <button className="btn btn-primary rounded-pill mr-1 px-3" onClick={() => alert("Share functionality here!")}>
-              <FontAwesomeIcon icon={faShare} />
-            </button>
-            <a className="btn btn-secondary rounded-pill px-3" href={`/edit/${data.id}`}>
-              <FontAwesomeIcon icon={faPencil} /> Edit Article
-            </a>
-          </div>
+            <p
+              className="dibuat mr-auto"
+              style={{ fontSize: "15px", maxWidth: "100%", paddingLeft: "10px", borderLeft: "3px solid #424347" }}
+            >
+              <span style={{ color: "var(--primary)" }}>Ditulis oleh</span> {data.Pembuat}{" "}
+              {data.Waktu ? ` pada ${data.Waktu}` : " pada 07-Maret-2023"}
+              {data.Diedit && (
+                <>
+                  <br />
+                  <span style={{ color: "var(--secondary)" }}>Diedit oleh</span> {data.Diedit}
+                </>
+              )}
+            </p>
 
-          <div className="text-justify w-100">
-            {data.Content.map((bab: any, index: any) => (
-              <div className="my-4" key={index}>
-                <h3>{bab.babTitle}</h3>
-                <p id={bab.babTitle} className="text-justify" dangerouslySetInnerHTML={{ __html: bab.babContent }}></p>
-              </div>
-            ))}
+            <div className="d-flex flex-column flex-md-row gap-3">
+              {data.Image && (
+                <img
+                  className="mr-2 cover"
+                  style={{
+                    height: "250px",
+                    objectFit: "contain",
+                    background: "rgba(0, 0, 0, 0)",
+                    borderRadius: "12px",
+                  }}
+                  src={data.Image}
+                  alt={data.Title}
+                />
+              )}
+
+              {data.Link && (
+                <article>
+                  <iframe
+                    style={{
+                      width: "460px",
+                      maxWidth: "100%",
+                      height: "250px",
+                      objectFit: "cover",
+                      borderRadius: "12px",
+                    }}
+                    className="img-fluid"
+                    src={data.Link}
+                    title={data.Title}
+                  ></iframe>
+                </article>
+              )}
+            </div>
+
+            <div className="d-flex mt-3 gap-2">
+              <button
+                className="btn btn-primary rounded-pill mr-1 px-3"
+                onClick={() => alert("Share functionality here!")}
+              >
+                <FontAwesomeIcon icon={faShare} />
+              </button>
+              <a className="btn btn-secondary rounded-pill px-3" href={`/edit/${data.id}`}>
+                <FontAwesomeIcon icon={faPencil} /> Edit Article
+              </a>
+            </div>
+
+            <div className="text-justify w-100">
+              {data.Content.map((bab: any, index: any) => (
+                <div className="my-4" key={index}>
+                  <h3>{bab.babTitle}</h3>
+                  <p id={bab.babTitle} className="text-justify" dangerouslySetInnerHTML={{ __html: bab.babContent }}></p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
